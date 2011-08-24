4d41dafeb764f2dc2a419dfc54674b8d4e622284
Ferenc Fodor <fodor0205@gmail.com>
2011-08-24 21:04:52 +0200

    Added basic edit tests

--- test/model/node.test.js
+++ test/model/node.test.js
@@ -24,3 +24,34 @@ exports['Node get level'] = function(test) {
 
   test.done()
 }
+
+exports['Node edit headline'] = function(test) {
+  test.expect(4)
+
+  test.equal(typeof node.editHeadline, 'function')
+  test.doesNotThrow(function() {
+    node.editHeadline(null, '', function(err) {
+      test.equal(err, 'A node ID szám kell hogy legyen!')
+    })
+    node.editHeadline(1, null, function(err) {
+      test.equal(err, 'Az új tartalom String kell hogy legyen!')
+    })
+  })
+
+  test.done()
+}
+exports['Node edit body'] = function(test) {
+  test.expect(4)
+
+  test.equal(typeof node.editBody, 'function')
+  test.doesNotThrow(function() {
+    node.editBody(null, '', function(err) {
+      test.equal(err, 'A node ID szám kell hogy legyen!')
+    })
+    node.editBody(1, null, function(err) {
+      test.equal(err, 'Az új tartalom String kell hogy legyen!')
+    })
+  })
+
+  test.done()
+}
