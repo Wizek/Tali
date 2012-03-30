789f48c79226e9010cfaf265a05c6a0cd1c8c779
Wizek <123.wizek@gmail.com>
2012-03-30 20:50:09 +0200

    Connected client with server on new node + edit headline.

--- app/socket.js
+++ app/socket.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 
 /**
  * Module dependencies
@@ -247,3 +248,282 @@ io.sockets.on('connection', function (socket) {
     socket.on(e, fn[e])
   }
 })
+=======
+
+/**
+ * Module dependencies
+ */
+var app = require('./expressServer')
+  , io = require('socket.io').listen(app)
+  , log = require('./log')
+  , user = require('./model/user')
+  , node = require('./model/node')
+  , helpers = require('./helpers')
+
+/**
+ * Socket.IO basic configuration
+ */
+io.configure('production', function() {
+  io.enable('browser client minification')
+  io.enable('browser client etag')
+  io.set('log level', 1)
+})
+
+io.set('log level', 0)
+
+/**
+ * Authorization before accepting a new connection to the editor namespace
+ * Emits a 'connect_failed' event on client side, if the new connection
+ * is refused by the server, or emits a 'connect' event on the client side,
+ * if the new connection is accepted and ready
+ */
+/*io.of('/editor').authorization(function(handshakeData, cb) {
+  cookies = helpers.cookiesToObject(handshakeData.headers.cookie)
+  if(envId = cookies.eID) {
+    user.isLoggedIn(envId, function(err, isLoggedIn) {
+      if (err) {
+        return cb()
+      } else {
+        if (isLoggedIn) {
+          handshakeData.envId = envId
+          return cb(null, true)
+        }
+        return cb()
+      }
+    })
+  }
+}).on('connection', function(socket) {
+  
+})*/
+
+
+
+  
+//})
+
+io.sockets.on('connection', function (socket) {
+  var fn = {}
+  /**
+   * Setting environment (browser) ID on the beginning of a connection
+   * If this browser was already logged in, it returns true otherwise an error
+   * @param envId {String} environment (browser) Id
+   * @param cb {function} cb(err, isLoggedIn)
+   */
+  fn['set envId'] = function(envId, cb) {
+    socket.set('envId', envId, function() {
+      user.isLoggedIn(envId, function(err, isLoggedIn) {
+        if (err) {
+          return cb(err)
+        } else {
+          if (isLoggedIn) {
+            for(var e in afterAuth) if (afterAuth.hasOwnProperty(e)) {
+              socket.on(e, afterAuth[e])
+            }
+            user.resume(envId, socket.id, function(err, username, userId, onlineList) {
+              socket.set('username', username)
+              socket.set('userId', userId)
+              socket.broadcast.emit('user joined', username, userId)
+            })
+            return cb(null, true)
+          } else {
+            return cb('Not logged in yet')
+          }
+        }
+      })
+    })
+  }
+
+  /**
+   * User login attempt with a username and a password
+   * If the login is successful, it stores some info, broadcasts a
+   * 'user joined' event, and initilializating the afterAuth functions,
+   * then returns the userId to this username
+   * @param username {String}
+   * @param password {String} Password unhashed
+   * @param cb {function} cb(err, userId)
+   */
+  fn['login'] = function(username, password, cb) {
+    socket.get('envId', function(err, envId) {
+      user.login(username, password, envId, socket.id, function(err, userId) {
+        if (err) {
+          return cb(err)
+        } else {
+          socket.set('username', username)
+          socket.set('userId', userId)
+          // todo
+          for (var e in afterAuth) if (afterAuth.hasOwnProperty(e)) {
+            socket.on(e, afterAuth[e])
+          }
+          return cb(err, userId)
+        }
+      })
+    })
+  }
+
+  /**
+   * User registration attempt
+   * If the registration is successful, it returns the newly generated userId
+   * otherwise a string with the error
+   * @param username {String}
+   * @param password {String}
+   * @param email {String}
+   * @param cb {function} cb(err, userId)
+   */
+  fn['register'] = function(username, password, email, cb) {
+    user.register(username, password, email, cb)
+  }
+
+  /**
+   * Bind the functions to the Socket.IO events
+   */
+  for (var e in fn) if (fn.hasOwnProperty(e)) {
+    socket.on(e, fn[e])
+  }
+
+
+  var afterAuth = {}
+
+  /**
+   * Set the focus to a node
+   * @param nodeId {Number}
+   * @param cb {function} cb(err)
+   */
+  afterAuth['set focus'] = function(nodeId, cb) {
+    socket.get('username', function(err, username) {
+      user.setFocus(nodeId, username, cb)
+      socket.broadcast.emit('change focus', nodeId, username)
+    })
+  }
+
+  /**
+   * Gets the childrens of a node
+   * @param nodeId {Number}
+   * @param cb {function} cb(err, results)
+   */
+  afterAuth['get children of'] = function(nodeId, cb) {
+    node.getLevel(nodeId, cb)
+  }
+
+  /**
+   * Makes a new node
+   * If the aboveId is 0 or null, then the new node will be the first in the list,
+   * if the aboveId is the Id of the last node, then the new node will be the last node
+   * If the make is successful, it broadcasts a 'new node' event, with the
+   * parentId, nodeId, nodePosition and the username
+   * @param parentId {Number}
+   * @param aboveId {Number} id of the node above the new node
+   * @param cb {function} cb(err, nodeId, nodePosition)
+   */
+  afterAuth['new node'] = function(parentId, aboveId, cb) {
+    socket.get('userId', function(err, userId) {
+      socket.get('username', function(err, username) {
+        node.newNode(parentId, aboveId, userId, function(err, nodeId, nodePosition) {
+          socket.broadcast.emit('new node', parentId, nodeId, nodePosition, username)
+          return cb(err, nodeId, nodePosition)
+        })
+      })
+    })
+  }
+  // * shortcut
+  afterAuth['new node by position'] = function(parentId, position, cb) {
+    socket.get('userId', function(err, userId) {
+      socket.get('username', function(err, username) {
+        node._s_newNode(parentId, position, userId, function(err, nodeId, nodePosition) {
+          socket.broadcast.emit('new node', parentId, nodeId, nodePosition, username)
+          return cb(err, nodeId, nodePosition)
+        })
+      })
+    })
+  }
+
+  /**
+   * Locks a node for editing
+   * @param nodeId {Number}
+   * @param cb {function} cb(err)
+   */
+  afterAuth['lock'] = function(nodeId, cb) {
+    socket.set('lock', nodeId)
+    socket.get('username', function(err, username) {
+      user.lock(nodeId, username, cb)
+      socket.broadcast.emit('lock', nodeId, username)
+    })
+  }
+
+  /**
+   * Unlocks the currently locked node
+   * @param cb {function} cb(err)
+   */
+  afterAuth['unlock'] = function(cb) {
+    socket.set('lock', null)
+    socket.get('username', function(err, username) {
+      user.unlock(username, cb)
+      socket.broadcast.emit('unlock', username)
+    })
+  }
+
+  /**
+   * Edits the headline of the currently focused node
+   * @param newText {String} The new content
+   * @param cb {function} cb(err)
+   */
+  afterAuth['edit headline'] = function(newText, cb) {
+    socket.get('userId', function(err, userId) {
+      socket.get('lock', function(err, lockId) {
+        node.editHeadline(lockId, newText, userId, cb)
+        socket.get('username', function(err, username) {
+          socket.broadcast.emit('change headline', lockId, newText, username)
+        })
+      })
+    })
+  }
+
+  // * shortcut
+  afterAuth['edit headline of node'] = function(newText, nodeId, cb) {
+    socket.get('userId', function(err, userId) {
+      node.editHeadline(nodeId, newText, userId, cb)
+      socket.get('username', function(err, username) {
+        socket.broadcast.emit('change headline', nodeId, newText, username)
+      })
+    })
+  }
+
+  /**
+   * Edits the body of the currently focused node
+   * @param newText {String} The new content
+   * @param cb {function} cb(err)
+   */
+  afterAuth['edit body'] = function(newText, cb) {
+    socket.get('userId', function(err, userId) {
+      socket.get('lock', function(err, lockId) {
+        node.editBody(lockId, newText, userId, cb)
+        socket.get('username', function(err, username) {
+          socket.broadcast.emit('change body', lockId, newText, username)
+        })
+      })
+    })
+  }
+
+  /**
+   * Current user is disconnecting
+   * This function broadcasts a 'user left' event
+   */
+  afterAuth['disconnect'] = function() {
+    socket.get('username', function(err, username) {
+      socket.broadcast.emit('user left', username)
+      user.disconnect(socket.id)
+    })
+  }
+
+
+  /**
+   * Current user is logging out
+   * @param cb {function} cb(err)
+   */
+  afterAuth['logout'] = function(cb) {
+    socket.get('envId', function(err, envId) {
+      user.logout(envId, cb)
+    })
+  }
+
+})
+>>>>>>> Stashed changes
--- www/Backbone/main4.js
+++ www/Backbone/main4.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 var sio = io.connect()
   , socket = sio.socket;
  
@@ -430,4 +431,549 @@ void function() {
   // console.log(focus.at().view.el)
 
   window.topLevel = new Children(null, {parent:null})
+=======
+void function() {
+  window.iAvg = Math.iAvg = function(a,b) {
+    Math.abs(a-b) < 10
+    return Math.round((a+b)/2)
+  }
+  var tplStr = {
+    node: _.template(''
+      // +'<span class="line">'
+        +'<span class="handle plus">+</span>'
+        +'<span class="handle minus">-</span>'
+        +'<span class="handle dot">&nbsp;</span>'
+        +'<textarea class="headline"><%= headline %></textarea>'
+        +'<span class="focusedBy"></span>'
+      // +'</span>'
+      +'<textarea class="body"><%= body %></textarea>')
+  }
+
+
+  // io.connect('/editor')
+  // console.log(io)
+  // io.on('connect', function() {
+  //   console.log('yupiiii', arguments)
+  //   io.emit('asd')
+  // })
+  Backbone.sync = function() {
+    console.log('Backbone.sync', arguments)
+  }
+  window.MAX_POS = Math.pow(2,32)-1
+  window.MIN_POS = 0
+  window.MID_POS = iAvg(MIN_POS, MAX_POS)
+  window.Node = Backbone.Model.extend(
+    { defaults: function() {
+        var o =
+          { headline: ''
+          , body: ''
+          , created_at: new Date()
+          , updated_at: new Date()
+          , position: MID_POS
+          , children: null
+          , expanded: true
+          , locked: false
+          , focus: null
+          }
+        return o
+      }
+    , initialize: function() {
+      if (this.get('childnum') > 0) {
+        var self = this
+        window.socket.emit('get children of', this.get('id'), function(err, results) {
+          console.log('get children of cb', arguments)
+          for (var i in results) {
+            var n = new Node(results[i])
+            // n.isNew = false
+            self.appendChild(n)
+          }
+        })
+        topLevel.cache[this.get('id')] = this
+      }
+      this.view = new NodeView({model:this})
+      this.set({children:new Children(null, {parent:this})})
+      this.bind('change:body', updateUpdatedAt)
+      this.bind('change:headline', function() {
+        socket.emit('edit headline of node', this.get('headline'), this.get('id'), function() {
+          console.log('edit headline of node cb', arguments)
+        })
+      })
+      this.bind('change:headline', updateUpdatedAt)
+      this.bind('change:position', updateUpdatedAt)
+      //this.bind('change:id', function(){console.log('ID VÁLTOZOTT', arguments)})
+      function updateUpdatedAt (self) {
+        self.set({'updated_at':new Date()})
+      }
+    }
+    , prevPos: function() {
+      var pos = this.get('position')
+      var coll = this.collection
+      var t = coll.reduce(function(a, b) {
+        var ap = a.get('position')
+        var bp = b.get('position')
+        if (bp < pos && bp > ap) {
+          return b
+        } else {
+          return a
+        }
+      }, new Node({position:MIN_POS}))
+      return t.get('position')
+    }
+    , nextPos: function() {
+      var pos = this.get('position')
+      var coll = this.collection
+      var t = coll.reduce(function(a, b) {
+        var ap = a.get('position')
+        var bp = b.get('position')
+        if (bp > pos && bp < ap) {
+          return b
+        } else {
+          return a
+        }
+      }, new Node({position:MAX_POS}))
+      return t.get('position')
+    }
+    , prevNode: function() {
+      return this.collection.getByPosition(this.prevPos())
+    }
+    , nextNode: function() {
+      return this.collection.getByPosition(this.nextPos())
+    }
+    , before: function(node) {
+      var pos = this.get('position')
+      var coll = this.collection
+      newPos = iAvg(this.prevPos(), pos)
+      coll.add(node)
+      node.set({position:newPos})
+    }
+    , after: function(node) {
+      var pos = this.get('position')
+      var coll = this.collection
+      newPos = iAvg(pos, this.nextPos())
+      coll.add(node)
+      node.set({position:newPos})
+    }
+    , appendChild: function(node) {
+      this.get('children').append(node)
+    }
+    , moveInto: function(ch) {
+      this.collection.remove(this)
+      ch.append(this)
+    }
+    , moveUp: function() {
+      var p = this.prevNode()
+      if (!p) {return}
+      var pp = p.prevNode() || new Node({position:MIN_POS})
+      this.set({position:iAvg(p.get('position'), pp.get('position'))})
+    }
+    , moveDown: function() {
+      var n = this.nextNode()
+      if (!n) {return}
+      var nn = n.nextNode() || new Node({position:MAX_POS})
+      this.set({position:iAvg(n.get('position'), nn.get('position'))})
+    }
+    , moveIn: function() {
+      var prev = this.prevNode()
+      if (!prev) {return}
+      this.collection.remove(this)
+      prev.get('children').append(this)
+    }
+    , moveOut: function() {
+      var par = this.collection.parent
+      if (!par) {return}
+      this.collection.remove(this)
+      par.after(this)
+    }
+    , childCount: function() {
+      return this.get('children').size()
+    }
+    , collapse: function() {
+      console.log(this)
+      this.set({expanded:false})
+    }
+    , expand: function() {
+      this.set({expanded:true})
+    }
+    , delete: function() {
+      this.destroy()
+    }
+  })
+
+  window.Children = Backbone.Collection.extend({
+    model: Node
+    , initialize: function(models, opts) {
+      this.parent = opts ? opts.parent || null : null
+      this.view = new ChildrenView({collection:this})
+    }
+    , getByPosition: function(position) {
+      return this.find(function(node) {
+        return node.get('position') == position
+      }) || null
+    }
+    , append: function(node) {
+      if (this.size()) {
+        var last = this.max(function(v) {return v.get('position')}).get('position')
+        node.set({position:iAvg(last, MAX_POS)})
+      }
+      this.add(node)
+    }
+    , first: function() {
+      return this.min(function(v) {
+        return v.get('position')
+      })
+    }
+    , last: function() {
+      return this.max(function(v) {
+        return v.get('position')
+      })
+    }
+  })
+
+  window.NodeView = Backbone.View.extend({
+    tagName: 'li'
+    , initialize: function() {
+      _.bindAll(this, 'render')
+      this.model.bind('change:headline', this.changeViewHead, this)
+      this.model.bind('change:body', this.changeViewBody, this)
+      this.model.bind('change:position', this.changeViewPosition, this)
+      this.model.bind('change:expanded', this.changeViewExpanded, this)
+      this.model.bind('change:focus', this.changeViewFocus, this)
+      this.model.bind('destroy', this.destroy, this)
+      this.render()
+      $(this.el).find('> .headline').mousedown(this.changeModelFocus.bind(this))
+    }
+    , events: {
+      'change .headline': 'changeModelHead',
+      'keyup .headline': 'changeModelHead',
+      'blur .headline': 'changeModelHead',
+      'change .body': 'changeModelBody',
+      'keyup .body': 'changeModelBody',
+      'blur .body': 'changeModelBody',
+      //'click .plus': 'changeModelFocus',
+    }
+    , changeViewExpanded: function() {
+      var e = this.model.get('expanded')
+      var t = this.model.get('children').view.$el
+      t.removeClass(e?'hidden':'visible')
+      t.addClass(e?'visible':'hidden')
+    }
+    , changeViewPosition: function() {
+      var coll = this.model.collection
+      if (!coll) {
+        return console.warn('Changed position of an orphan node', this)
+      }
+      var prev = this.model.prevNode()
+      if (prev) { 
+        prev.view.$el.after(this.$el)
+      } else {
+        this.model.collection.view.$el.prepend(this.$el)
+      }
+    }
+    , changeModelHead: function() {
+      // TODO not just change event
+      var text = this.$el.children('.headline').val()
+      this.model.set({headline: text})
+    }
+    , changeModelBody: function() {
+      var text = this.$el.children('.body').val()
+      this.model.set({body: text})
+    }
+    , changeModelFocus: function() {
+      focus.at(this.model)
+    }
+    , changeViewHead: function() {
+      // console.log('changeViewHead', this.model.get('headline'))
+      this.$el.children('.headline').val(this.model.get('headline'))
+    }
+    , changeViewBody: function() {
+      this.$el.children('.body').val(this.model.get('body'))
+    }
+    , changeViewFocus: function(node, focus) {
+      var f = node.get('focus')
+      if (!f) {
+        this.$el.removeClass('focused ours theirs')
+      } else {
+        this.$el.addClass('focused '+(f.get('ours')?'ours':'theirs'))
+      }
+    }
+    , render: function() {
+      this.$el.html(tplStr.node(this.model.toJSON()))
+    }
+    , destroy: function() {
+      this.$el.remove()
+    }
+  })
+
+  window.ChildrenView = Backbone.View.extend({
+    tagName: 'ul',
+    initialize: function() {
+      var self = this
+      this.collection.bind('add', function(node) {
+        $(self.collection.view.el).append(node.view.el)
+      })
+      this.render()
+    },
+    render: function() {
+      var p = this.collection.parent
+      if (p) {
+        $(this.el).addClass(p.get('expanded') ? 'visible' : 'hidden')
+        $(p.view.el).append(this.el)
+      }
+    }
+    // , 
+  })
+  window.Interface = Backbone.View.extend({
+    className: 'tali-top'
+    , tagName: 'div'
+    , initialize: function () {
+      window.title = 'Tali WIP' 
+      _.bindAll(this, 'render')
+      this.render()
+      var socket = window.socket = io.connect('http://localhost:3000');
+      socket.on('connect', function (data) {
+        //console.info('sucessfully established a connection with the namespace')
+        tryToConnect()
+        function tryToConnect () {
+          socket.emit('set envId', 'eg14g14g14241g1', function(err, isLoggedIn) {
+            if (err) {
+              console.log('még nem voltam belépve')
+              console.log('belépek')
+              socket.emit('login', 'Fodi69', 'mypass', function(err, userId) {
+                if (err) {
+                  console.error(err)
+                } else {
+                  console.log('sikeres belépés', userId)
+                  tryToConnect()
+                }
+              });
+            } else {
+              if (isLoggedIn) {
+                console.log('már be voltam lépve')
+                socket.on('user joined', function(username, userId) {
+                  var el = $('<div class="notification">').text(username + ' csatlakozott.')
+                  el.prependTo('#notifications')
+                  setTimeout(function() {
+                    el.remove()
+                  }, 5000)
+                })
+                socket.on('user left', function(username, userId) {
+                  var el = $('<div class="notification">').text(username + ' szétkapcsolt.')
+                  el.prependTo('#notifications')
+                  setTimeout(function() {
+                    el.remove()
+                  }, 5000)
+                })
+                socket.on('change focus', function(nodeId, username) {
+                  var c = topLevel.cache[nodeId]
+                  if (!asd[username]) {
+                    asd[username] = new Focus({ours:false, username:username})
+                  }
+                  asd[username].at(c)
+                  //f.at(c)
+                  console.log(arguments)
+                })
+                socket.on('change headline', function(nodeId, newHeadline, username) {
+                  topLevel.cache[nodeId].set('headline', newHeadline)
+                })
+                socket.emit('get children of', 0, function(err, results) {
+                  console.log('get children of cb')
+                  for (var i in results) {
+                    var n = new Node(results[i])
+                    if (i == 0) {
+                      focus.at(n)
+                    }
+                    // n.isNew = false
+                    topLevel.append(n)
+                  }
+                  //console.log('bóó', arguments)
+                })
+              }
+            }
+          })
+        }
+      })
+
+    }
+    , render:function() {
+      $(this.el).append('<div id="notifications"></div>')
+    }
+  })
+
+  window.Focus = Backbone.Model.extend({
+    defaults: {
+      at: null,
+      ours: false,
+    },
+    initialize: function() {
+      this.view = new FocusView({model:this})
+      _.bindAll(this, 'at')
+      // console.log(this)
+      // _.bind(this.goUp, this)
+      this.bind('change:at', this.atChanged)
+    },
+    atChanged: function(focus, at) {
+      socket.emit('set focus', at.get('id'), function() {
+        console.log('set focus cb', arguments)
+      })
+      // * shortcut
+      //socket.emit('lock', at.get('id'), function() {
+      //  console.log(arguments)
+      //})
+      at.set({focus:focus})
+      var prev = focus.previousAttributes()
+      if (prev.at) {
+        prev.at.set({focus:null})
+      }
+    },
+    at: function(v) {
+      // console.log(this)
+      if (arguments.length) {
+        if (!v) {return}
+        if (v.get('focus')) {
+          throw new Error('Trying to override focus', this, v)
+        }
+        this.set({at:v})
+        return v
+      } else {
+        return this.get('at')
+      }
+    },
+    goUp: function() {
+      // console.log(this)
+      this.at(this.at().prevNode())
+    },
+    goDown: function() {
+      this.at(this.at().nextNode())
+    },
+    goIn: function() {
+      this.at(this.at().get('children').first())
+    },
+    goOut: function() {
+      this.at(this.at().collection.parent)
+    },
+    // goFlatUp: function() {}, 
+    // goFlatDown: function() {}, 
+    addNodeAfterAndFocusIt: function() {
+      var t = this.at
+      var n = new Node
+      t().after(n)
+      var parentId = t().collection.parent? t().collection.parent.get('id') :0
+      // debugger
+      //if (t().isNew) {
+        console.log('emitting new node by position'
+          , parentId, n.get('position'))
+        socket.emit('new node by position', parentId
+            , n.get('position'), function(err, nodeId, nodePosition) {
+          console.log('new node by position cb', arguments)
+          n.set('id', nodeId)
+          // if (!err) {
+          //   n.isNew = false
+          // }
+          t(n)
+        })
+      //}
+    },
+    deleteNodeAndFocusPrevious: function() {
+      var t = this.at()
+      var prevHead = t.prevNode().get('headline')
+      var ourHead = t.get('headline')
+      t.prevNode().set({headline: prevHead+ourHead})
+      this.at(t.prevNode())
+      t.delete()
+      return prevHead.length
+    }, 
+    deleteNodeAndFocusNext: function() {}, 
+    expandCurrentLevel: function() {
+      this.at().expand()
+    }, 
+    collapseCurrentLevel: function() {
+      this.at().collapse()
+    }, 
+    expandAndGoIn: function() {}, 
+    // goOutAndCollapse: function() {}, 
+    moveUp: function() {
+      this.at().moveUp()
+      this.view.focusHeadElement()
+    }, 
+    moveDown: function() {
+      this.at().moveDown()
+      this.view.focusHeadElement()
+    }, 
+    // moveFlatUp: function() {}, 
+    // moveFlatDown: function() {}, 
+    moveIn: function() {
+      this.at().moveIn()
+      this.view.focusHeadElement()
+    }, 
+    moveOut: function() {
+      this.at().moveOut()
+      this.view.focusHeadElement()
+    }, 
+    copyAfterAndFocusIt: function() {}, 
+  })
+  // Focus.prototype.goUp = Focus.prototype.goUp.bind(Focus.prototype)
+  // Focus.prototype.at = Focus.prototype.at.bind(Focus.prototype)
+
+  window.FocusView = Backbone.View.extend({
+    initialize: function() {
+      var m = this.model
+      _self = this
+      this.model.bind('change:at', this.focusHeadElement, this)
+      this.model.bind('destroy', this.destroy, this)
+      // _.bindAll(this, 'focusHeadElement')
+      // window.addEventListener('click', m.goUp.bind(m), true)
+      shortcut.add('up', m.goUp.bind(m))
+      shortcut.add('down', m.goDown.bind(m))
+      shortcut.add('alt+up', m.goUp.bind(m))
+      shortcut.add('alt+down', m.goDown.bind(m))
+      shortcut.add('alt+right', m.goIn.bind(m))
+      shortcut.add('alt+left', m.goOut.bind(m))
+      shortcut.add('enter', m.addNodeAfterAndFocusIt.bind(m))
+      
+      shortcut.add('backspace', function() {
+        console.log('Duty calls!', arguments)
+        var _ref
+        _ref = m.at().view.$el.find('> .headline').get(0)
+        console.log()
+        // If the cursore is at the beginning
+        if (_ref.selectionStart == 0 && _ref.selectionEnd == 0) {
+          var at = m.deleteNodeAndFocusPrevious.apply(m)
+          m.at().view.$el.find('> .headline').get(0).setSelectionRange(at, at)
+        }
+      }, {
+        propagate: true
+      })
+
+      // shortcut.add('delete', m.deleteNodeAndFocusNext.bind(m))
+      shortcut.add('alt+l', m.expandCurrentLevel.bind(m))
+      shortcut.add('alt+h', m.collapseCurrentLevel.bind(m))
+      shortcut.add('alt+shift+up', m.moveUp.bind(m))
+      shortcut.add('alt+shift+down', m.moveDown.bind(m))
+      shortcut.add('alt+shift+right', m.moveIn.bind(m))
+      shortcut.add('alt+shift+left', m.moveOut.bind(m))
+      shortcut.add('tab', m.moveIn.bind(m))
+      shortcut.add('shift+tab', m.moveOut.bind(m))
+      shortcut.add('ctrl+down', m.copyAfterAndFocusIt.bind(m))
+      /*\
+       *  Őket Hagyjuk békén:
+       *  shift jobb-bal-ra
+       *  ctrl jobb-bal-ra
+       *  ctrl-shift jobb-bal-ra
+       *  
+       *  
+       *  
+      \*/
+    },
+    focusHeadElement: function() {
+      $(this.model.at().view.el).find('> .headline').focus()
+    },
+  })
+
+  window.focus = new Focus({ours:true})
+  // console.log(focus.at().view.el)
+
+  window.topLevel = new Children(null, {parent:null})
+  window.topLevel.cache = {}
+  window.asd = {}
+>>>>>>> Stashed changes
 }()
